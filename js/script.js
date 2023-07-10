e291ae5dd9477d07542d477c3fcdc7f443c6d467
Karan Mahajan <iamkaranmaverick@gmail.com>
2023-07-09 22:23:49 -0400

    Populated data from API (#2)

--- js/script.js
+++ js/script.js
@@ -2,11 +2,18 @@ async function fetchData() {
     const res = await fetch("https://api.coronavirus.data.gov.uk/v1/data");
     const record = await res.json();
 
-    
+    var table = "" ;
+ 
+		for(var i=0; i < 100; i++)
+        {
+			table += "<tr>";
+			table += "<td>" + record.data[i].date +"</td>" 
+					+ "<td>" + record.data[i].areaName +"</td>" 
+					+ "<td>" + record.data[i].latestBy +"</td>"
+                    + "<td>" + record.data[i].deathNew +"</td>";
+			table += "</tr>";
+		}
 
-    document.getElementById("date").innerHTML = record.data[0].date;
-    document.getElementById("areaName").innerHTML = record.data[0].areaName;
-    document.getElementById("latestBy").innerHTML = record.data[0].latestBy;
-    document.getElementById("deathNew").innerHTML = record.data[0].deathNew;
+    document.getElementById("result").innerHTML = table;
 }
 fetchData();
